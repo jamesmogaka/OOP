5f6b24015139d05c33cdedcbf10977653398cc5b
james-mogaka <jamesoyondi23@gmail.com>
2022-04-15 16:57:33 +0300

    Object instantiation and attributes

--- oop.py
+++ oop.py
@@ -4,3 +4,11 @@ class student:
     pass
 
 
+student1 = student() #instantiation of an object
+student1.name = "james"
+student1.age = 22
+
+
+print (type(student1))
+print (type(student1.name))
+print (type(student1.age))
