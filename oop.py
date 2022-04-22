554320fa73ab29f1c700a7bd1af3d4c2003d360c
james-mogaka <jamesoyondi23@gmail.com>
2022-04-22 23:00:10 +0300

    methods to output attributes

--- oop.py
+++ oop.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 class student:
+    gender = "male"
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def out(self):
+        return f"Name: {self.name}\nAge: {self.age}\nGender: {self.gender}"
+
 
 student1 = student("james", 22) #instantiation of an object
-print(student1)
-print(student1.name, student1.age)
+print(student1.out())
